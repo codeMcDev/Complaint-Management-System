a5d67e6279910d5915039a4a4ae05547a7b7c27b
Mark Asamoah <mcasky16@gmail.com>
2026-04-07 22:56:56 +0200

    Prepared for production

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./configs/db.config.js";
 import complaintsRoutes from "./routes/complaints.route.js";
 import authRoutes from "./routes/auth.route.js";
+import path from "path";
 
 dotenv.config();
 
@@ -12,12 +13,22 @@ const server = express();
 //Defining the PORT
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 //middlewares
 server.use(express.json());
 
 //Routes
 server.use("/api/complaints", complaintsRoutes);
 server.use("/api/auth", authRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  server.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  server.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 //Start the server
 connectDB().then(() => {
